e08ddf8570bc630e8c83ba5349c2c8ea997374f7
Gen <kebikovgen@gmail.com>
2022-06-02 23:57:29 +0300

    123

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strice';
+
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c',
+};
+
+const b = 'name12';
+
+arrObj['b'] = '1234';
+
+console.log('',arrObj.name);
+console.log(arrObj['b']);
+
+внесены изминения yes
